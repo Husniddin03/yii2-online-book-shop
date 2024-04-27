1baf8203e64856d6cf1fed8a6dd8e7ea06e4386b
Husniddin03 <husniddin13124041@gmail.com>
2024-04-27 11:12:18 +0500

    Created index

--- yiifrem/frontend/assets/BookAsset.php
+++ yiifrem/frontend/assets/BookAsset.php
@@ -18,20 +18,12 @@ class BookAsset extends AssetBundle
         'bootstrap/css/bootstrap.css',
         'bootstrap/css/bootstrap.css.map',
         'bootstrap/css/bootstrap.min.css',
-        'bootstrap/css/bootstrap.min.css.map',
     ];
     public $js = [
         'js/jquery.min.js',
         'js/jquery.scrollex.min.js',
         'js/jquery.scrolly.min.js',
         'js/main.js',
-        'bootstrap/js/bootstrap.bundle.js',
-        // 'bootstrap/js/bootstrap.bundle.js.map',
         'bootstrap/js/bootstrap.bundle.min.js',
-        // 'bootstrap/js/bootstrap.bundle.min.js.map',
-        'bootstrap/js/bootstrap.js',
-        // 'bootstrap/js/bootstrap.js.map',
-        'bootstrap/js/bootstrap.min.js',
-        // 'bootstrap/js/bootstrap.min.js.map',
     ];
 }
--- yiifrem/frontend/views/layouts/main.php
+++ yiifrem/frontend/views/layouts/main.php
@@ -19,8 +19,9 @@ BookAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    
 </head>
-<body class="d-flex flex-column h-100">
+<body class="">
 <?php $this->beginBody() ?>
 
 <div id="wrapper">
@@ -29,7 +30,7 @@ BookAsset::register($this);
             <div class="inner">
 
                 <!-- Logo -->
-                    <a href="index.html" class="logo">
+                    <a href="/book/index" class="logo">
                         <span class="fa fa-book"></span> <span class="title">Book Online Store Website</span>
                     </a>
 
@@ -69,6 +70,7 @@ BookAsset::register($this);
                 <li><a href="/site/index">Home</a></li>
                 <li><a href="/site/about">About</a></li>
                 <li><a href="/site/contact">Contact</a></li>
+                <li><a href="/book/index">Books</a></li>
                 <li><a href="/site/signup">Signup</a></li>
                 <li><a href="/site/login">Login</a></li>
                 <li><a href="/site/logout">Logout</a></li>
@@ -76,21 +78,68 @@ BookAsset::register($this);
         </nav>
 
     <!-- Main -->
+    <?= Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+    <?= Alert::widget() ?>
+    <?= $content ?>
 
-    <main role="main" class="flex-shrink-0">
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
-    </main>
+<!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <section>
+                <h2>Contact Us</h2>
+                <form method="post" action="#">
+                    <div class="fields">
+                        <div class="field half">
+                            <input type="text" name="name" id="name" placeholder="Name" />
+                        </div>
 
-    <footer class="footer mt-auto py-3 text-muted">
-        <div class="container">
-            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-            <p class="float-end"><?= Yii::powered() ?></p>
+                        <div class="field half">
+                            <input type="text" name="email" id="email" placeholder="Email" />
+                        </div>
+
+                        <div class="field">
+                            <input type="text" name="subject" id="subject" placeholder="Subject" />
+                        </div>
+
+                        <div class="field">
+                            <textarea name="message" id="message" rows="3" placeholder="Notes"></textarea>
+                        </div>
+
+                        <div class="field text-right">
+                            <label>&nbsp;</label>
+
+                            <ul class="actions">
+                                <li><input type="submit" value="Send Message" class="primary" /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </section>
+            <section>
+                <h2>Contact Info</h2>
+
+                <ul class="alt">
+                    <li><span class="fa fa-envelope-o"></span> <a href="mailto:husniddin13124041@email.com">husniddin13124041@gmail.com</a></li>
+                    <li><span class="fa fa-phone"></span> +998 (93) 129 1312</li>
+                    <li><span class="fa fa-map-pin"></span> 212 Barrington Court New York, ABC 10001 United States of America</li>
+                </ul>
+
+                <h2>Follow Us</h2>
+
+                <ul class="icons">
+                    <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                </ul>
+            </section>
+
+            <ul class="copyright">
+                <li>Copyright © 2020 Company Name </li>
+                <li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
+            </ul>
         </div>
     </footer>
 </div>
